f247cc448372520f45dd838caeb31b50e0b23e3a
Muhammad Shofwan Qobus <81661542+shofwanqobus@users.noreply.github.com>
2021-11-30 08:50:04 +0700

    Add files via upload

---
+++ TUBES_MOBPRO/app/src/main/java/com/mobileprogramming/tubes_mobpro/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.mobileprogramming.tubes_mobpro.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobileprogramming.tubes_mobpro.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ TUBES_MOBPRO/app/src/main/java/com/mobileprogramming/tubes_mobpro/LogIn/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.mobileprogramming.tubes_mobpro.LogIn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobileprogramming.tubes_mobpro.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
